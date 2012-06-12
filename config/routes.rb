01e3bd01d7ff28838bba5aeea1af0b8388aea4b6
adarapata <efb.youdie@gmail.com>
2012-06-12 17:51:17 +0900

    add to header

--- config/routes.rb
+++ config/routes.rb
@@ -2,8 +2,11 @@ Bookmark::Application.routes.draw do
   resources :users
   resources :sessions, only: [:new, :create, :destroy]
 
+  root to: 'static_pages#home'
+
   match '/signup', to: 'users#new'
   match '/signin', to: 'sessions#new'
+  match '/signout', to: 'sessions#destroy', via: :delete
 
   # The priority is based upon order of creation:
   # first created -> highest priorit
