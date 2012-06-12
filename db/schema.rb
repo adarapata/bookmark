723cc03b709fdf59798a147b778536e8d868f3ad
Kihaco <kiha@paperboy.co.jp>
2012-06-12 11:42:39 +0900

    title wo japanese ni shitayo

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended to check this file into your version control system.
 
-ActiveRecord::Schema.define(:version => 20120611104317) do
+ActiveRecord::Schema.define(:version => 20120611104523) do
 
   create_table "users", :force => true do |t|
     t.string   "name"
