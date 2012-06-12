037fe535b63b3c59a349619992e84c7cda32325c
adarapata <efb.youdie@gmail.com>
2012-06-12 11:29:03 +0900

    Userモデルのspec作成

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,3 +1,6 @@
 class User < ActiveRecord::Base
   attr_accessible :id, :lastupdate, :name, :pass
+  
+  validates :name, presence: true, length: { maximum: 50 }
+  validates :pass, presence: true
 end
--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -1,5 +1,30 @@
 require 'spec_helper'
 
 describe User do
-  pending "add some examples to (or delete) #{__FILE__}"
+
+  before { @user = User.new(name: "Test", pass: "aaaaaa") }
+
+  subject { @user }
+
+  it { should respond_to(:name) }
+  it { should respond_to(:pass) }
+  it { should respond_to(:lastupdate) }
+
+  it { should be_valid }
+  
+  describe "when name is not present" do
+    before { @user.name = " " }
+    it { should_not be_valid }
+  end
+
+  describe "when pass is not present" do
+    before { @user.pass = " " }
+    it { should_not be_valid }
+  end
+  
+  describe "when name is too long" do
+    before { @user.name = "a" * 51 }
+    it { should_not be_valid }
+  end
+
 end
