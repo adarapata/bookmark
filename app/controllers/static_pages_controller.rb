773222389f0268d6feab430896cb2de345386b07
adarapata <efb.youdie@gmail.com>
2012-06-14 15:46:38 +0900

    errored bookmarks class

--- app/controllers/static_pages_controller.rb
+++ app/controllers/static_pages_controller.rb
@@ -4,5 +4,9 @@ class StaticPagesController < ApplicationController
     @user = User.new
     @e = Entry.all
     @entries = @e.reverse
+    
+    if signed_in?
+       @bookmarks = Bookmark.where("user_id = ?", current_user.id).reverse
+    end
   end
 end
