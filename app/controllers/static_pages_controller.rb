8c3af39983e4f0df6256fc52d80c1bc0d6dea871
adarapata <efb.youdie@gmail.com>
2012-06-14 18:07:43 +0900

    mybookmark show for home.html

--- app/controllers/static_pages_controller.rb
+++ app/controllers/static_pages_controller.rb
@@ -1,8 +1,11 @@
-
 class StaticPagesController < ApplicationController
   def home
     @user = User.new
     @e = Entry.all
     @entries = @e.reverse
+    
+    if signed_in?
+       @bookmarks = Bookmark.where("user_id = ?", current_user.id).reverse
+    end
   end
 end
---
+++ app/models/bookmark.rb
@@ -0,0 +1,9 @@
+class Bookmark < ActiveRecord::Base
+  attr_accessible :comment, :entry_id, :user_id
+
+  belongs_to :entry
+
+  validates :entry_id, presence: true
+  validates :user_id, presence: true
+
+end
