4fd42afafe24d82654cd1bb40ecc00f7e62ec091
adarapata <efb.youdie@gmail.com>
2012-06-14 17:15:30 +0900

    application rename Bookmark=>AriakeBook

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -1,4 +1,4 @@
-Bookmark::Application.configure do
+AriakeBook::Application.configure do
   # Settings specified here will take precedence over those in config/application.rb
 
   # Code is not reloaded between requests
