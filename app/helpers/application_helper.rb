f5f4b91381f0e13f12b328ae7ef8b6d1235d3a40
adarapata <efb.youdie@gmail.com>
2012-06-15 13:57:32 +0900

    fix title

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,7 +1,7 @@
 module ApplicationHelper
   # Returns the full title on a per-page basis
   def full_title(page_title)
-    base_title = "GBM"
+    base_title = "Game Bookmark"
     if page_title.empty?
       base_title
     else
